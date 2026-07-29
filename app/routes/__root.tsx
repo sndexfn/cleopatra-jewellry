9c50838bb0bdb4aa5088e1277f6a49b43be9aa34
sndexfn <gono5566778899@gmail.com>
2026-07-30 02:26:55 +0300

    Update __root.tsx

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,7 +1,8 @@
 import { createRootRoute, Outlet, ScrollRestoration } from '@tanstack/react-router';
 import { Meta, Scripts } from '@tanstack/start';
 import React from 'react';
-import '../../src/styles.css'; // ربط ملف الألوان الفاخرة
+import '../../src/styles.css';
+import { Navbar } from '../components/Navbar';
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -11,10 +12,17 @@ function RootComponent() {
   return (
     <React.Fragment>
       <Meta />
-      {/* سيتم إضافة شريط التنقل (Navbar) هنا لاحقاً */}
-      <main className="min-h-screen bg-cleopatra-deepSea text-gray-100 flex flex-col">
-        <Outlet />
-      </main>
+      <div className="min-h-screen bg-cleopatra-deepSea text-gray-100 flex flex-col font-sans">
+        {/* شريط التنقل العلوي الفاخر */}
+        <Navbar />
+        
+        {/* مكان شريط أسعار الذهب اللحظية (سنقوم ببرمجته وإضافته هنا قريباً) */}
+        
+        {/* محتوى الصفحات */}
+        <main className="flex-grow flex flex-col">
+          <Outlet />
+        </main>
+      </div>
       <ScrollRestoration />
       <Scripts />
     </React.Fragment>
