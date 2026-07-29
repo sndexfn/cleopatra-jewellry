7eb1c6b262540f8c7d159b855d1eb7fec1aa2787
sndexfn <gono5566778899@gmail.com>
2026-07-30 02:15:50 +0300

    Delete app/app/client.tsx

--- app/app/client.tsx
+++
@@ -1,9 +0,0 @@
-import { StartClient } from '@tanstack/start';
-import { createRoot } from 'react-dom/client';
-import { createRouter } from './router';
-
-const router = createRouter();
-
-createRoot(document.getElementById('root')!).render(
-  <StartClient router={router} />
-);
